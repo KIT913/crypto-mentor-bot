c988aa89c618dadca5cc22e0c02f5bee176e1320
KIT913 <kit38098@gmail.com>
2025-05-17 16:49:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,54 @@
 import telebot
 from flask import Flask, request
-import os
+import threading
 
 API_TOKEN = '7831525545:AAE2ndGlPoZ1vQWHmPW24aKDwCoWrhguP-g'
+admin_id = 7928644968
+
 bot = telebot.TeleBot(API_TOKEN)
-server = Flask(__name__)
+app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def welcome(message):
+    user = message.from_user
     chat_id = message.chat.id
-    text = (
-        "🖐 Glad to see you on our👉🏻 *CHANNEL* 👈🏻\n\n"
-        "We are the «*CRYPTO MENTOR | KIT grp*» community💎\n\n"
-        "📊 *Our team is based on Futures market analysis.*\n"
-        "*The winning percentage of our Signals is now 97%* ✅\n\n"
-        "📲 [Join now](https://t.me/+lxFWpKJ49J8wN2Vh)"
+
+    # Уведомление админу
+    notify_admin = (
+        f"🚨 New user launched the bot!\n"
+        f"Name: {user.first_name} {user.last_name or ''}\n"
+        f"Username: @{user.username}\n"
+        f"User ID: {user.id}"
+    )
+    bot.send_message(admin_id, notify_admin)
+
+    # Сообщение клиенту
+    caption = (
+        "🖐🏼 Glad to see you on our👉🏻 CHANNEL 👈🏻\n\n"
+        "We are the «CRYPTO MENTOR | KIT grp» community\n\n"
+        "📊 Our team is based on Futures market analysis.\n"
+        "The winning percentage of our Signals is now 97% ✅\n\n"
+        "📲 Join now"
     )
 
     markup = telebot.types.InlineKeyboardMarkup()
     markup.row(
-        telebot.types.InlineKeyboardButton("CHANNEL➜📲", url="https://t.me/+lxFWpKJ49J8wN2Vh"),
-        telebot.types.InlineKeyboardButton("GET FREE SIGNAL", url="https://t.me/Kit_futures?text=I%20WANT%20SIGNAL")
+        telebot.types.InlineKeyboardButton("CHANNEL➜📲", url="https://t.me/+V8FKWfygiRY4Nzcx"),
+        telebot.types.InlineKeyboardButton("GET FREE SIGNAL✅", url="https://t.me/Kit_futures?text=I%20WANT%20SIGNAL")
     )
+    bot.send_message(chat_id, caption, reply_markup=markup)
 
-    bot.send_message(chat_id, text, parse_mode="Markdown", reply_markup=markup)
-
-@server.route(f"/{API_TOKEN}", methods=["POST"])
-def webhook():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return {"ok": True}
-
-@server.route("/")
+# Flask + keep alive
+@app.route('/')
 def index():
-    return "Bot is live!"
+    return 'Bot is running!'
 
-bot.remove_webhook()
-bot.set_webhook(url=f"https://crypto-mentor-bot.onrender.com/{API_TOKEN}")
+def run_flask():
+    app.run(host='0.0.0.0', port=8080)
 
-if name == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+def run_bot():
+    bot.infinity_polling()
+
+if name == '__main__':
+    threading.Thread(target=run_flask).start()
+    run_bot()
