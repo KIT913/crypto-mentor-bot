edad27f423d1f88c71533b78b3e86380c5ee4ce5
KIT913 <kit38098@gmail.com>
2025-05-17 17:40:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import telebot
+from keep_alive import keep_alive
 
-bot = telebot.TeleBot("7831525545:AAE2ndGlPoZ1vQWHmPW24aKDwCoWrhguP-g")
+API_TOKEN = "7831525545:AAE2ndGlPoZ1vQWHmPW24aKDwCoWrhguP-g"
 admin_id = 7928644968
 
+bot = telebot.TeleBot(API_TOKEN)
+
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def welcome(message):
     user = message.from_user
-    text = (
-        "👋🏼 Glad to see you on our👉🏼 <b>CHANNEL</b> 👈🏼\n\n"
-        "We are the «<b>CRYPTO MENTOR | KIT grp</b>» community💎\n\n"
-        "📊 Our team is based on Futures market analysis.\n"
-        "The winning percentage of our Signals is now <b>97%</b> ✅\n\n"
-        "📲 <a href='https://t.me/+lxFWpKJ49J8wN2Vh'>Join now</a>"
-    )
-    bot.send_message(message.chat.id, text, parse_mode='HTML')
-    
-    # Уведомление админу
-    notify = f"👤 New user launched the bot!\n▪️ Name: {user.first_name}\n▪️ Username: @{user.username}\n▪️ ID: {user.id}"
+    notify = f"""
+New user launched the bot!
+
+Username: @{user.username}
+ID: {user.id}
+Name: {user.first_name} {user.last_name or ""}
+"""
+
     bot.send_message(admin_id, notify)
 
+    welcome_text = """👋🏼 Glad to see you on our👉🏼 CHANNEL👈🏼
+
+We are the «CRYPTO MENTOR | KIT grp» community💎
+
+📊 Our team is based on Futures market analysis.
+The winning percentage of our Signals is now 97% ✅
+
+📲 [Join now](https://t.me/Kit_futures?text=I%20WANT%20SIGNAL)
+"""
+    bot.send_message(message.chat.id, welcome_text, parse_mode="Markdown")
+
+keep_alive()
 bot.polling()
