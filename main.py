2a67b607def5bcb74e09d2da8035b09e952790fd
KIT913 <kit38098@gmail.com>
2025-05-17 15:52:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import telebot
-from keep_alive import keep_alive
 
-bot = telebot.TeleBot("7831525545:AAE2ndGlPoZ1vQWHmPW24aKDwCoWrhguP-g", parse_mode="Markdown")
-
-admin_id = 7928644968  # твой Telegram ID
+bot = telebot.TeleBot("7831525545:AAE2ndGlPoZ1vQWHmPW24aKDwCoWrhguP-g")
+admin_id = 7928644968
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
@@ -12,30 +10,28 @@ def send_welcome(message):
 
     # Уведомление админу
     notify = (
-        f"👤 *New user launched the bot!*\n"
-        f"▪️ Name: {user.first_name}\n"
-        f"▪️ Username: @{user.username if user.username else '—'}\n"
-        f"▪️ ID: `{user.id}`"
+        f"👤 New user launched the bot!\n"
+        f"First name: {user.first_name} {user.last_name or ''}\n"
+        f"Username: @{user.username}\n"
+        f"ID: {user.id}"
     )
     bot.send_message(admin_id, notify)
 
-    # Текст пользователю
-    caption = (
-        "✋🏼Glad to see you on our👉🏼 [CHANNEL](https://t.me/+V8FKWfygiRY4Nzcx)👈🏼\n\n"
-        "We are the [«CRYPTO MENTOR | KIT grp»](https://t.me/+V8FKWfygiRY4Nzcx) community💎\n\n"
-        "*📊 Our team is based on Futures market analysis.*\n"
+    # Сообщение пользователю
+    text = (
+        "🖐 Glad to see you on our👉🏻 *CHANNEL* 👈🏻\n\n"
+        "We are the «*CRYPTO MENTOR | KIT grp*» community💎\n\n"
+        "📊 *Our team is based on Futures market analysis.*\n"
         "*The winning percentage of our Signals is now 97%* ✅\n\n"
-        "📲 [Join now](https://t.me/Kit_futures?text=I%20WANT%20SIGNAL)"
+        "📲 [Join now](https://t.me/+lxFWpKJ49J8wN2Vh)"
     )
 
-    # Кнопки
     markup = telebot.types.InlineKeyboardMarkup()
     markup.row(
-        telebot.types.InlineKeyboardButton("CHANNEL📲", url="https://t.me/+V8FKWfygiRY4Nzcx"),
-        telebot.types.InlineKeyboardButton("GET FREE SIGNAL🤑", url="https://t.me/Kit_futures?text=I%20WANT%20SIGNAL")
+        telebot.types.InlineKeyboardButton("CHANNEL➜📲", url="https://t.me/+lxFWpKJ49J8wN2Vh"),
+        telebot.types.InlineKeyboardButton("GET FREE SIGNAL", url="https://t.me/Kit_futures?text=I%20WANT%20SIGNAL")
     )
 
-    bot.send_message(chat_id, caption, reply_markup=markup)
+    bot.send_message(chat_id, text, parse_mode="Markdown", reply_markup=markup)
 
-keep_alive()
-bot.polling()
+bot.polling(none_stop=True)
